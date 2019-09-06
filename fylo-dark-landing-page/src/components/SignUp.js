ad3a45c9a91a4152f9aab372ae4f259b6ea33167
moniet <moniet.sawhney@gmail.com>
2019-09-06 22:40:01 +0530

    made testimonial page

---
+++ fylo-dark-landing-page/src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import Para from './Para'
+
+const Container = styled.div`
+
+`
+
+const SignUp = () => (
+    <Container>
+        
+    </Container>
+)
