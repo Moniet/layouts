7732f4018b41d39c394676cc82e09e61f50b15e1
moniet <moniet.sawhney@gmail.com>
2019-09-17 13:17:22 +0530

    Updates

---
+++ precache-manifest.c5865480f94405328fe130fdcc0df482.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f158f65f42e9d1875c8b9e9b209dc168",
+    "url": "/Moniet/fylo-dark.git/index.html"
+  },
+  {
+    "revision": "3118966a7f883b4ac1ef",
+    "url": "/Moniet/fylo-dark.git/static/css/main.c5d2e969.chunk.css"
+  },
+  {
+    "revision": "e0d87b6d38cc445abf4c",
+    "url": "/Moniet/fylo-dark.git/static/js/2.fb404810.chunk.js"
+  },
+  {
+    "revision": "3118966a7f883b4ac1ef",
+    "url": "/Moniet/fylo-dark.git/static/js/main.55cd518a.chunk.js"
+  },
+  {
+    "revision": "725d54d7aad8cbf0b7e8",
+    "url": "/Moniet/fylo-dark.git/static/js/runtime~main.806c8342.js"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/Moniet/fylo-dark.git/static/media/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "c096d1166611171e131cb663b7a211dc",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-facebook.c096d116.svg"
+  },
+  {
+    "revision": "662aa75267a1ef880381c492d88d44d7",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-instagram.662aa752.svg"
+  },
+  {
+    "revision": "0f65814bd9f282828254e3382e223a57",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-location.0f65814b.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "05cd782a5fcba3dab1376914fa234f25",
+    "url": "/Moniet/fylo-dark.git/static/media/icon-twitter.05cd782a.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/Moniet/fylo-dark.git/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/Moniet/fylo-dark.git/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/Moniet/fylo-dark.git/static/media/logo.c71bf7bf.svg"
+  }
+]);
